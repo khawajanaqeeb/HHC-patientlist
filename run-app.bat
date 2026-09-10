d811acebf614bc3f3043f8d3740619b9dc7cbfe3
khawaja naqeeb <shajiekns@yahoo.com>
2026-09-11 02:35:44 +0500

    Configure run-app.bat to launch in clean browser app mode

--- run-app.bat
+++ run-app.bat
@@ -5,6 +5,6 @@ cd /d "f:\client-projects\HHC-patientlist"
 echo Starting HHC Patient Visit Sheet...
 echo Opening application in your browser...
 
-powershell -Command "Start-Sleep -Seconds 2; Start-Process 'http://localhost:3000'"
+powershell -Command "Start-Sleep -Seconds 2; Start-Process msedge '-app=http://localhost:3000'"
 
 npm run dev
